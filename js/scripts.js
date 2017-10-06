827708c685fe66c6e1a996072063f9d4757eb78b
madisonwalls <madilyn@live.unc.edu>
2017-10-06 18:08:25 -0400

    Final Commit

--- js/scripts.js
+++ js/scripts.js
@@ -10,10 +10,73 @@ window.onclick = function(event) {
   var links = document.getElementsByClassName("dropdown-items");
     var i;
     for (i = 0; i < links.length; i++) {
-      var open = link[i];
+      var open = links[i];
       if (openDropdown.classList.contains('show')) {
         openDropdown.classList.remove('show');
       }
     }
   }
+}
+
+//map
+
+var x = 1;
+
+function mapPopulate() {
+  console.log(x)
+  console.log("circle" + x);
+  if ((x%3) === 0 ) {
+  document.getElementById("circle" + x).style.fill = "#E0303D";
+  }
+  x = x + 1;
+
+}
+
+
+//drag elements
+
+function revealStart(event) {
+    event.dataTransfer.setData("Text", event.target.id);
+}
+
+function revealStart1(event) {
+    event.dataTransfer.setData("Text", event.target.id);
+}
+function revealStart2(event) {
+    event.dataTransfer.setData("Text", event.target.id);
+}
+
+function allowReveal(event) {
+    event.preventDefault();
+}
+
+function allowReveal1(event) {
+    event.preventDefault();
+}
+
+function allowReveal2(event) {
+    event.preventDefault();
+}
+
+function reveal(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("Text");
+    event.target.appendChild(document.getElementById(data));
+    document.getElementById("revealed-text").innerHTML = "Wi-fi</br> Router";
+}
+
+
+function reveal1(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("Text");
+    event.target.appendChild(document.getElementById(data));
+    document.getElementById("revealed-text1").innerHTML = "Internet</br>Activation";
+}
+
+
+function reveal2(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("Text");
+    event.target.appendChild(document.getElementById(data));
+    document.getElementById("revealed-text2").innerHTML = "NO</br> Contract";
 }
